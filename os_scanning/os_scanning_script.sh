a37b010b8cc0883f6ee06afea60ac42d35590522
Yang Li <66520315+Yang-Li86@users.noreply.github.com>
2023-06-13 12:14:23 +0100

    Add OS scanning script

---
+++ os_scanning/os_scanning_script.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#using nmap for os scanning
+
+
+#using nmap for the os test
+# Check if Nmap is installed
+if ! command -v nmap &> /dev/null
+then
+    echo "Nmap is not installed. Please install Nmap first."
+    exit
+fi
+
+# Read target IP address from user input
+ip_address=$1
+
+# Run Nmap scan on target IP address
+nmap -O $ip_address
